057f4739fa631d489b83ecaf7285ec566d30fa38
Gabriel-Aguiar <gabrielaguiarjob@gmail.com>
2021-05-18 20:35:52 -0300

    Aula 06

---
+++ Ex06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex06
+
+ {
+
+     //Objetivo do progrma calcular a área do retangulo
+     //Data....; 18-05-2021
+     //Programador: Gabriel-Aguiar
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Declaraçao de variaveis
+            double baseRet, altura, area;
+            
+           
+
+           Console.Write("Digite a base do retagulo:  ");
+            baseRet =  double.Parse( Console.ReadLine());
+
+            Console.Write("Digite a base do retagulo:  ");
+            altura =  double.Parse( Console.ReadLine());
+
+
+            //Formula do Calculo da area: Produto da altura pela base
+
+            area = baseRet * altura;
+            Console.Write("A area do retangulo é: " + area);
+  
+
+        }
+    }
+}
